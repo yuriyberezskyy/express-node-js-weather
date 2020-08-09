81fefe4d6fdb825aa4fdabfb52025fefde6eca86
yuriyberezskyy <bereza95@gmail.com>
2020-08-09 18:17:13 -0400

    Added more html files and work with routers

--- src/app.js
+++ src/app.js
@@ -8,22 +8,18 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
+// app.get("/help", (req, res) => {
+//   res.send([
+//     {
+//       name: "Andrew",
+//       age: 27,
+//     },
+//   ]);
+// });
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Andrew",
-      age: 27,
-    },
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About page</h1>");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About page</h1>");
+// });
 
 app.get("/weather", (req, res) => {
   res.send([{ forecast: "50F", location: "New York City" }]);

88d32d9c79832ee74c08e1c7d85fd17b2fed5d8a
yuriyberezskyy <bereza95@gmail.com>
2020-08-09 18:10:38 -0400

    added html and connected to node

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const express = require("express");
 
+console.log(__dirname);
+console.log(path.join(__dirname, "../public"));
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("", (req, res) => {
   res.send("<h1>Weather</h1>");
@@ -16,11 +22,11 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send("<h1>About page</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Your Weather");
+  res.send([{ forecast: "50F", location: "New York City" }]);
 });
 
 app.listen(3000, () => {

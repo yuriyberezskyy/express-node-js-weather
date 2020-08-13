04841d270e64565aa6813353efa6dde6b8ed3145
yuriyberezskyy <bereza95@gmail.com>
2020-08-13 13:43:29 -0400

    added some css and js files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const path = require("path");
 const express = require("express");
 
-console.log(__dirname);
-console.log(path.join(__dirname, "../public"));
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));

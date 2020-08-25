1dcf02c5e12662a8a0acb6699a762434d9f0a06a
yuriyberezskyy <bereza95@gmail.com>
2020-08-25 16:49:18 -0400

    created event listener

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,18 @@
-fetch(`http://localhost:3000/weather?address=boston`) //eslint-disable-line
-  .then((response) => response.json())
-  .then((res) => {
-    if (res.error) {
-      console.log(res.error);
-    } else {
-      console.log(res);
-    }
-  });
+const weahterForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weahterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const location = search.value;
+  console.log(location);
+
+  fetch("http://localhost:3000/weather?address=" + location) //eslint-disable-line
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.error) {
+        console.log(res.error);
+      } else {
+        console.log(res);
+      }
+    });
+});

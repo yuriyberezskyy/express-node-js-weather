b6823738f54ae2824b7868b21fded40870d02bdd
yuriyberezskyy <bereza95@gmail.com>
2020-08-24 20:46:14 -0400

    done with query request

--- src/app.js
+++ src/app.js
@@ -35,7 +35,26 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.send([{ forecast: "50F", location: "New York City" }]);
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide the address!",
+    });
+  }
+  res.send([
+    { forecast: "50F", location: "New York City", address: req.query.address },
+  ]);
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
 });
 
 app.get("/help/*", (req, res) => {

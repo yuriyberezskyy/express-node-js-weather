0c620d7826f2193c43b8bfdee332ed431da5002a
yuriyberezskyy <bereza95@gmail.com>
2020-08-25 16:27:48 -0400

    fetched from my Api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-console.log("Client side in javascript");
+fetch(`http://localhost:3000/weather?address=boston`) //eslint-disable-line
+  .then((response) => response.json())
+  .then((res) => {
+    if (res.error) {
+      console.log(res.error);
+    } else {
+      console.log(res);
+    }
+  });
